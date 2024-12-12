5006ae69006e4819c744a3d1f8f07f59749513cc
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-12-12 18:29:50 +0300

    console.log(process.env);

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
+  console.log(process.env);
   console.log('App is started on:', await app.getUrl());
 }
 bootstrap();

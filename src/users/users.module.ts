f07b340a933f9fb7b2bb4660d46809578d8dd8e2
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-12-15 19:56:49 +0300

    added UserController

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
 import { PrismaModule } from 'src/db/prisma.module';
 import { JwtService } from './jwt.service';
+import { UserController } from './users.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [UserController],
   providers: [UserService, JwtService],
   exports: [UserService],
 })

f9b229897aaf3390a071febdee8a20c73e761d9f
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-12-26 17:34:21 +0300

    added jwtserv

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 import { PrismaService } from 'src/db/prisma.service';
+import { JwtService } from 'src/users/jwt.service';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService, PrismaService],
+  providers: [TagsService, PrismaService, JwtService],
 })
 export class TagsModule {}
